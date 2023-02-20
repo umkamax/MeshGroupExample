6c49be3dc46d12860da843fcc3033c866314c1c3
maksim.shilin <maksim.shilin@nvi-solutions.com>
2023-02-20 08:26:27 +0300

    SpringBoot added.

--- src/main/java/ru/meshgroup/example/MeshGroupExampleApplication.java
+++ src/main/java/ru/meshgroup/example/MeshGroupExampleApplication.java
@@ -1,4 +1,13 @@
 package ru.meshgroup.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MeshGroupExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeshGroupExampleApplication.class, args);
+    }
+
 }
--- src/test/java/ru/meshgroup/example/MeshGroupExampleApplicationTest.java
+++ src/test/java/ru/meshgroup/example/MeshGroupExampleApplicationTest.java
@@ -1,4 +1,15 @@
+package ru.meshgroup.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class MeshGroupExampleApplicationTest {
-  
+
+    @Test
+    void contextLoads() {
+    }
+
 }
